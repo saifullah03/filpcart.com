19b544a34861231f1ad19b370934042137c66076
SUCCESS <khalid.saifullah0012@gmail.com>
2025-07-01 08:32:08 +0600

    This is my first commit

---
+++ src/main/java/com/microtech/FlipCart/FlipCartApplication.java
@@ -0,0 +1,13 @@
+package com.microtech.FlipCart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipCartApplication.class, args);
+	}
+
+}
