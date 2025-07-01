68ddf65a8bc0d69d22fd3013134478147c547304
SUCCESS <khalid.saifullah0012@gmail.com>
2025-07-01 23:04:52 +0600

    Hello Objects

--- src/main/java/com/microtech/FlipCart/entity/Demo.java
+++ src/main/java/com/microtech/FlipCart/entity/Demo.java
@@ -6,7 +6,6 @@ public class Demo {
         System.out.println("Java");
         System.out.println("Its github hooks with jenkins");
         System.out.println("Hi hello");
-        System.out.println("Hell this is Khaled Saifullah");
-        System.out.println("Hell this is Khaled Saifullah");
+        System.out.println("Hello this is Khaled Saifullah");
     }
 }
