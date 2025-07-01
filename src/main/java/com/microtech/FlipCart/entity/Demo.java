24b0056d532bcef1041af9c06f975e363e826d38
SUCCESS <khalid.saifullah0012@gmail.com>
2025-07-01 22:32:49 +0600

    This my webhooks commit

--- src/main/java/com/microtech/FlipCart/entity/Demo.java
+++ src/main/java/com/microtech/FlipCart/entity/Demo.java
@@ -3,6 +3,6 @@ package com.microtech.FlipCart.entity;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello I am Java");
-        System.out.println("Java);
+        System.out.println("Java");
     }
 }
