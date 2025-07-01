aec76a6dfcaa6265b78bdf128cd298bb70dcbcba
SUCCESS <khalid.saifullah0012@gmail.com>
2025-07-01 22:53:21 +0600

    Hello Commit new

--- src/main/java/com/microtech/FlipCart/entity/Demo.java
+++ src/main/java/com/microtech/FlipCart/entity/Demo.java
@@ -7,5 +7,6 @@ public class Demo {
         System.out.println("Its github hooks with jenkins");
         System.out.println("Hi hello");
         System.out.println("Hell this is Khaled Saifullah");
+        System.out.println("Hell this is Khaled Saifullah");
     }
 }
